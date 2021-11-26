b7cbbebf30be051c7fe5ff973a9881f368ac888e
Kayden Lee <kayden@programmer.net>
2021-11-26 21:19:25 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import "./styles.css";
 import Sidebar from "./Sidebar";
+import Home from "./Home";
 
 export default function App() {
     return (
       <div className="App">
-        
+        <Home />
 
         <Sidebar />
       </div>
